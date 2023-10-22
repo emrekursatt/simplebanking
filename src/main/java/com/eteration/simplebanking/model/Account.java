a38b3529c67a04f9cc8b7eb081c5e42afefa0543
Emre Kürşat <104613938+emrekursatt@users.noreply.github.com>
2023-10-22 23:17:45 +0300

    revize

--- src/main/java/com/eteration/simplebanking/model/Account.java
+++ src/main/java/com/eteration/simplebanking/model/Account.java
@@ -2,7 +2,6 @@ package com.eteration.simplebanking.model;
 
 
 import com.eteration.simplebanking.exception.InsufficientBalanceException;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
